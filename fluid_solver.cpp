bd0db8000ca3bb9dd89f3ae93cac8ccab6bc8e40
Hugo Gomes pg57549@alunos.uminho.pt <pg57549@search7edu.local>
2024-11-17 12:12:55 +0000

    codigo todo paralelizado, acho que neste momento nao da para mais

--- fluid_solver.cpp
+++ fluid_solver.cpp
@@ -4,25 +4,17 @@
 #include <cstdint>
 
 #define IX(i, j, k) ((i) + (M + 2) * (j) + (M + 2) * (N + 2) * (k))
-inline void SWAP(float *&x0, float *&x)                                        
-  {                                                                           
-    float *tmp = x0;                                                           
-    x0 = x;                                                                    
-    x = tmp;                                                                   
-  }
-// Usar uma função inline em vez de uma macro para o MAX
-inline float MAX(float a, float b) {
-  return (a > b) ? a : b;
-}
+void SWAP(float *&x0, float *&x) { float *tmp = x0; x0 = x; x = tmp;}
+float MAX(float a, float b) { return (a > b) ? a : b; }
 #define LINEARSOLVERTIMES 20
 
 // Add sources (density or velocity)
 void add_source(int M, int N, int O, float *x, float *s, float dt) {
   int size = (M + 2) * (N + 2) * (O + 2);
-  int i = 0;
 
   // Unroll the loop by a factor of 16
-  for (; i <= size - 16; i += 16) {
+  #pragma omp parallel for
+  for (int i = 0; i <= size - 16; i += 16) {
     x[i] += dt * s[i];
     x[i + 1] += dt * s[i + 1];
     x[i + 2] += dt * s[i + 2];
@@ -42,7 +34,8 @@ void add_source(int M, int N, int O, float *x, float *s, float dt) {
   }
 
   // Handle the remaining elements
-  for (; i < size; i++) {
+  #pragma omp parallel for
+  for (int i = size - (size % 16); i < size; i++) {
     x[i] += dt * s[i];
   }
 }
@@ -52,28 +45,47 @@ void set_bnd(int M, int N, int O, int b, float *x) {
   int i, j;
 
   // Atualizar os limites das faces Z (k = 0 e k = O + 1)
+  #pragma omp parallel for collapse(2) private(i, j)
   for (j = 1; j <= N; j++) {
-    for (i = 1; i <= M; i++) {
+    for (i = 1; i <= M; i += 4) { // Loop Unrolling com passo de 4
       x[IX(i, j, 0)] = (b == 3) ? -x[IX(i, j, 1)] : x[IX(i, j, 1)];
+      x[IX(i + 1, j, 0)] = (b == 3) ? -x[IX(i + 1, j, 1)] : x[IX(i + 1, j, 1)];
+      x[IX(i + 2, j, 0)] = (b == 3) ? -x[IX(i + 2, j, 1)] : x[IX(i + 2, j, 1)];
+      x[IX(i + 3, j, 0)] = (b == 3) ? -x[IX(i + 3, j, 1)] : x[IX(i + 3, j, 1)];
+
       x[IX(i, j, O + 1)] = (b == 3) ? -x[IX(i, j, O)] : x[IX(i, j, O)];
+      x[IX(i + 1, j, O + 1)] = (b == 3) ? -x[IX(i + 1, j, O)] : x[IX(i + 1, j, O)];
+      x[IX(i + 2, j, O + 1)] = (b == 3) ? -x[IX(i + 2, j, O)] : x[IX(i + 2, j, O)];
+      x[IX(i + 3, j, O + 1)] = (b == 3) ? -x[IX(i + 3, j, O)] : x[IX(i + 3, j, O)];
     }
   }
 
   // Atualizar os limites das faces X (i = 0 e i = M + 1)
+  #pragma omp parallel for collapse(2) private(i, j)
   for (j = 1; j <= O; j++) {
-    for (i = 1; i <= N; i++) {
+    for (i = 1; i <= N; i += 2) {
       x[IX(0, i, j)] = (b == 1) ? -x[IX(1, i, j)] : x[IX(1, i, j)];
+      x[IX(0, i + 1, j)] = (b == 1) ? -x[IX(1, i + 1, j)] : x[IX(1, i + 1, j)];
+
       x[IX(M + 1, i, j)] = (b == 1) ? -x[IX(M, i, j)] : x[IX(M, i, j)];
+      x[IX(M + 1, i + 1, j)] = (b == 1) ? -x[IX(M, i + 1, j)] : x[IX(M, i + 1, j)];
     }
   }
 
+
   // Atualizar os limites das faces Y (j = 0 e j = N + 1)
+  #pragma omp parallel for collapse(2) private(i, j)
   for (i = 1; i <= M; i++) {
-    for (j = 1; j <= O; j++) {
+    for (j = 1; j <= O; j += 2) {
       x[IX(i, 0, j)] = (b == 2) ? -x[IX(i, 1, j)] : x[IX(i, 1, j)];
+      x[IX(i, 0, j + 1)] = (b == 2) ? -x[IX(i, 1, j + 1)] : x[IX(i, 1, j + 1)];
+
       x[IX(i, N + 1, j)] = (b == 2) ? -x[IX(i, N, j)] : x[IX(i, N, j)];
+      x[IX(i, N + 1, j + 1)] = (b == 2) ? -x[IX(i, N, j + 1)] : x[IX(i, N, j + 1)];
     }
   }
+
+
 
   // Atualizar os cantos
   x[IX(0, 0, 0)] = 0.33f * (x[IX(1, 0, 0)] + x[IX(0, 1, 0)] + x[IX(0, 0, 1)]);
@@ -82,8 +94,7 @@ void set_bnd(int M, int N, int O, int b, float *x) {
   x[IX(M + 1, N + 1, 0)] = 0.33f * (x[IX(M, N + 1, 0)] + x[IX(M + 1, N, 0)] + x[IX(M + 1, N + 1, 1)]);
 }
 
-
-
+// Aux Function of lin_solve
 float calculate_new_value(int i, int j, int k, float *x, float *x0, float a, float c, int M, int N, int O) {
   int M2 = M + 2;
   int N2 = N + 2;
@@ -112,7 +123,7 @@ void lin_solve(int M, int N, int O, int b, float *x, float *x0, float a, float c
         #pragma omp parallel
         {
         // Red phase
-        #pragma omp for collapse(2) reduction(max:max_c) private(old_x, change) //schedule(dynamic)
+        #pragma omp for collapse(2) reduction(max:max_c) private(old_x, change)
         for (int kk = 1; kk <= O; kk += blockSize) {
             for (int jj = 1; jj <= N; jj += blockSize) {
                 int k_max = std::min(kk + blockSize, O + 1);
@@ -120,7 +131,7 @@ void lin_solve(int M, int N, int O, int b, float *x, float *x0, float a, float c
                 
                 for (int k = kk; k < k_max; k++) {
                     for (int j = jj; j < j_max; j++) {
-                        int start_i = 1 + (j + k) % 2;
+                        int start_i = 1 + (j + k) % 2; // Red cells start
                         
                         #pragma omp simd aligned(x:64)
                         for (int i = start_i; i <= M; i += 2) {
@@ -136,11 +147,11 @@ void lin_solve(int M, int N, int O, int b, float *x, float *x0, float a, float c
             }
         }
 
-        // Ensure synchronization between red and black phases
+        // synchronization between red and black phases
         #pragma omp barrier
 
         // Black phase
-        #pragma omp for collapse(2) reduction(max:max_c) private(old_x, change) //schedule(dynamic)
+        #pragma omp for collapse(2) reduction(max:max_c) private(old_x, change) 
         for (int kk = 1; kk <= O; kk += blockSize) {
             for (int jj = 1; jj <= N; jj += blockSize) {
                 int k_max = std::min(kk + blockSize, O + 1);
@@ -177,64 +188,58 @@ void diffuse(int M, int N, int O, int b, float *x, float *x0, float diff, float 
 
 // Advection step (uses velocity field to move quantities)
 void advect(int M, int N, int O, int b, float *d, float *d0, float *u, float *v, float *w, float dt) {
-  float dtX = dt * M, dtY = dt * N, dtZ = dt * O;
+    float dtX = dt * M, dtY = dt * N, dtZ = dt * O;
 
-  // Definir tamanhos dos blocos
-  int block_size_i = 8;
-  int block_size_j = 8;
-  int block_size_k = 8;
+    int block_size_i = 8;
+    int block_size_j = 8;
+    int block_size_k = 8;
 
-  for (int kk = 1; kk <= O; kk += block_size_k) {
-    for (int jj = 1; jj <= N; jj += block_size_j) {
-      for (int ii = 1; ii <= M; ii += block_size_i) {
-        // Dentro de cada bloco, iterar sobre os elementos
-        for (int k = kk; k < kk + block_size_k && k <= O; k++) {
-          for (int j = jj; j < jj + block_size_j && j <= N; j++) {
-            for (int i = ii; i < ii + block_size_i && i <= M; i++) {
-              int idx = IX(i, j, k);
-              float x = i - dtX * u[idx];
-              float y = j - dtY * v[idx];
-              float z = k - dtZ * w[idx];
+    #pragma omp parallel for collapse(3)
+    for (int kk = 1; kk <= O; kk += block_size_k) {
+        for (int jj = 1; jj <= N; jj += block_size_j) {
+            for (int ii = 1; ii <= M; ii += block_size_i) {
+                // Iterar sobre os elementos dentro de cada bloco
 
-              // Clamping nas fronteiras da grade
-              if (x < 0.5f)
-                x = 0.5f;
-              if (x > M + 0.5f)
-                x = M + 0.5f;
-              if (y < 0.5f)
-                y = 0.5f;
-              if (y > N + 0.5f)
-                y = N + 0.5f;
-              if (z < 0.5f)
-                z = 0.5f;
-              if (z > O + 0.5f)
-                z = O + 0.5f;
+                for (int k = kk; k < kk + block_size_k && k <= O; k++) {
+                    for (int j = jj; j < jj + block_size_j && j <= N; j++) {
+                        for (int i = ii; i < ii + block_size_i && i <= M; i++) {
+                            int idx = IX(i, j, k);
+                            float x = i - dtX * u[idx];
+                            float y = j - dtY * v[idx];
+                            float z = k - dtZ * w[idx];
 
-              int i0 = (int)x, i1 = i0 + 1;
-              int j0 = (int)y, j1 = j0 + 1;
-              int k0 = (int)z, k1 = k0 + 1;
+                            // Clamping nas fronteiras da grade
+                            if (x < 0.5f) x = 0.5f;
+                            if (x > M + 0.5f) x = M + 0.5f;
+                            if (y < 0.5f) y = 0.5f;
+                            if (y > N + 0.5f) y = N + 0.5f;
+                            if (z < 0.5f) z = 0.5f;
+                            if (z > O + 0.5f) z = O + 0.5f;
 
-              float s1 = x - i0, s0 = 1 - s1;
-              float t1 = y - j0, t0 = 1 - t1;
-              float u1 = z - k0, u0 = 1 - u1;
+                            int i0 = (int)x, i1 = i0 + 1;
+                            int j0 = (int)y, j1 = j0 + 1;
+                            int k0 = (int)z, k1 = k0 + 1;
 
-              d[idx] =
-                  s0 * (t0 * (u0 * d0[IX(i0, j0, k0)] + u1 * d0[IX(i0, j0, k1)]) +
-                        t1 * (u0 * d0[IX(i0, j1, k0)] + u1 * d0[IX(i0, j1, k1)])) +
-                  s1 * (t0 * (u0 * d0[IX(i1, j0, k0)] + u1 * d0[IX(i1, j0, k1)]) +
-                        t1 * (u0 * d0[IX(i1, j1, k0)] + u1 * d0[IX(i1, j1, k1)]));
+                            float s1 = x - i0, s0 = 1 - s1;
+                            float t1 = y - j0, t0 = 1 - t1;
+                            float u1 = z - k0, u0 = 1 - u1;
+
+                            d[idx] = s0 * (t0 * (u0 * d0[IX(i0, j0, k0)] + u1 * d0[IX(i0, j0, k1)]) +
+                                          t1 * (u0 * d0[IX(i0, j1, k0)] + u1 * d0[IX(i0, j1, k1)])) +
+                                    s1 * (t0 * (u0 * d0[IX(i1, j0, k0)] + u1 * d0[IX(i1, j0, k1)]) +
+                                          t1 * (u0 * d0[IX(i1, j1, k0)] + u1 * d0[IX(i1, j1, k1)]));
+                        }
+                    }
+                }
             }
-          }
         }
-      }
     }
-  }
-  set_bnd(M, N, O, b, d);
+
+    set_bnd(M, N, O, b, d);
 }
 
 
-// Projection step to ensure incompressibility (make the velocity field
-// divergence-free)
+// Projection step to ensure incompressibility (make the velocity field divergence-free)
 void project(int M, int N, int O, float *u, float *v, float *w, float *p, float *div) {
   int M2 = M + 2;
   int N2 = N + 2;
@@ -245,9 +250,11 @@ void project(int M, int N, int O, float *u, float *v, float *w, float *p, float 
   int block_size_k = 8;
 
   // Primeira parte: cálculo de div e inicialização de p com loop unrolling
+  #pragma omp parallel for collapse(3)
   for (int kk = 1; kk <= O; kk += block_size_k) {
     for (int jj = 1; jj <= N; jj += block_size_j) {
       for (int ii = 1; ii <= M; ii += block_size_i) {
+
         for (int k = kk; k < kk + block_size_k && k <= O; k++) {
           for (int j = jj; j < jj + block_size_j && j <= N; j++) {
             int i;
@@ -282,6 +289,7 @@ void project(int M, int N, int O, float *u, float *v, float *w, float *p, float 
   lin_solve(M, N, O, 0, p, div, 1, 6);
 
   // Segunda parte: atualização de u, v e w com loop unrolling
+  #pragma omp parallel for collapse(3)
   for (int kk = 1; kk <= O; kk += block_size_k) {
     for (int jj = 1; jj <= N; jj += block_size_j) {
       for (int ii = 1; ii <= M; ii += block_size_i) {
@@ -316,21 +324,18 @@ void project(int M, int N, int O, float *u, float *v, float *w, float *p, float 
 }
 
 
-
-
 // Step function for density
-void dens_step(int M, int N, int O, float *x, float *x0, float *u, float *v,
-               float *w, float diff, float dt) {
-  add_source(M, N, O, x, x0, dt);
-  SWAP(x0, x);
-  diffuse(M, N, O, 0, x, x0, diff, dt);
-  SWAP(x0, x);
-  advect(M, N, O, 0, x, x0, u, v, w, dt);
+void dens_step(int M, int N, int O, float *x, float *x0, float *u, float *v, float *w, float diff, float dt) {
+    add_source(M, N, O, x, x0, dt);
+    SWAP(x0, x);
+    diffuse(M, N, O, 0, x, x0, diff, dt);
+    SWAP(x0, x);
+    advect(M, N, O, 0, x, x0, u, v, w, dt);
 }
 
+
 // Step function for velocity
-void vel_step(int M, int N, int O, float *u, float *v, float *w, float *u0,
-              float *v0, float *w0, float visc, float dt) {
+void vel_step(int M, int N, int O, float *u, float *v, float *w, float *u0, float *v0, float *w0, float visc, float dt) {
   add_source(M, N, O, u, u0, dt);
   add_source(M, N, O, v, v0, dt);
   add_source(M, N, O, w, w0, dt);
