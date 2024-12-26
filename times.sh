9ddac6ee3ea260e6d7251bb92050726abe57ea85
Hugo Gomes pg57549@alunos.uminho.pt <pg57549@search7edu.local>
2024-12-26 23:44:27 +0000

    .

--- times.sh
+++ times.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
 #
-#SBATCH --exclusive 	  # exclusive node for the job
-#SBATCH --time=02:00      # allocation for 2 minutes
- 
-time ./fluid_sim_seq
-export OMP_NUM_THREADS=1
-time ./fluid_sim
-export OMP_NUM_THREADS=2
-time ./fluid_sim
-export OMP_NUM_THREADS=4
-time ./fluid_sim
-export OMP_NUM_THREADS=6
-time ./fluid_sim
-export OMP_NUM_THREADS=8
-time ./fluid_sim
-export OMP_NUM_THREADS=12
-time ./fluid_sim
-export OMP_NUM_THREADS=16
-time ./fluid_sim
-export OMP_NUM_THREADS=20
-time ./fluid_sim
+#SBATCH --exclusive      # exclusive node for the job
+#SBATCH --time=02:00     # allocation for 2 minutes
+
+# Run the sequential version first
+echo "Running fluid_sim_seq (1 thread)..."
+(time ./fluid_sim_seq)
+
+# Run fluid_sim with different thread counts
+for threads in 2 4 6 8 12 16 20 24 30 40
+do
+    echo "Running fluid_sim with $threads threads..." 
+    export OMP_NUM_THREADS=$threads
+    (time ./fluid_sim)
+done
